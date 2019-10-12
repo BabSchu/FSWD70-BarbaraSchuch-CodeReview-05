001e108f792a8569cbf79b5dbab35ef4c2683256
BabSchu <you@example.com>
2019-10-12 13:07:27 +0200

    add sort button and function

--- JS/myscript.js
+++ JS/myscript.js
@@ -1,9 +1,18 @@
 //movie information from json
 var movieCard = JSON.parse(movies);
 
-console.table(movieCard);
-
 $( document ).ready(function() {
+//sort by rating high to low
+	$("#sort").on('click', function(){
+		movieCard.sort(function(a, b){
+		    var a1= a.rating, b1= b.rating;
+		    if(a1== b1) return 0;
+		    return a1< b1? 1: -1;
+		});
+	 console.table(movieCard);
+
+	 });
+
 //add MovieCard
 	for (let i = 0; i < movieCard.length; i++) {
 		$(`<div class="col-12 col-md-6 px-5 mb-3">
@@ -17,7 +26,7 @@ $( document ).ready(function() {
 								<h5 class="card-title">${movieCard[i].title}</h5>
 								<p class="card-text">${movieCard[i].description}</p>
 								<div class="card-text d-flex flex-grow-1 align-items-end justify-content-end">
-									<button type=button class="btn p-0 d-flex align-items-center align-bottom mb-0">								
+									<button type=button class="lButton btn p-0 d-flex align-items-center align-bottom mb-0">								
 										<p class="text-success text-right mb-0 mr-1">Like
 											<i class="fas fa-thumbs-up"></i>
 										<div class="rounded-circle bg-success rating text-center text-light">
@@ -33,7 +42,7 @@ $( document ).ready(function() {
 	 }
 //increment rating
 
-	var likeBtn = $("button");
+	var likeBtn = $(".lButton");
 	var ratingValue = $(".ratingValue");
 
 	for(let i = 0; i < movieCard.length; i++){
